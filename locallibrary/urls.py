ffe0b566df6eae95f9a866c9e3d6845883b31320
seouuuukjin <uukjin4@gmail.com>
2021-01-04 22:12:26 +0900

    장고 공식문서 따라해보기 시작

---
+++ locallibrary/urls.py
@@ -0,0 +1,43 @@
+"""locallibrary URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import include
+from django.urls import path
+
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    
+    #localhost/catalog --> catalog/urls.py 를 통해서 매핑 하겠다는 의미
+    # Use include() to add paths from the catalog application
+    path('catalog/', include('catalog.urls')),
+    
+    #Add URL maps to redirect the base URL to our application
+    #그니까 지금 위에랑 밑에 이거랑 해서 "/catalog" 랑  "/"로 들어오는거 모두 "/catalog"
+    #로 처리해서, catalog/urls.py 통해서 처리하겠다는 의미
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Use static() to add url mapping to serve static files during development (only)
+
+
+
